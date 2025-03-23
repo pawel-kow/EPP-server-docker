76cbafbaf0879de9797c3a7a959b7e265645ddc8
Pawel Kowalik <2063440-pawel-kow@users.noreply.gitlab.com>
2025-03-23 17:29:29 +0100

    NEW FEATURE: registry conf generated from compose file

--- .docker/epp/docker_entrypoint.sh
+++ .docker/epp/docker_entrypoint.sh
@@ -3,6 +3,9 @@
 # Replace MySQL username and password in registry.conf
 sed -i "s/<MYSQL_USER>/$MYSQL_USER/g" /etc/registry/registry.conf
 sed -i "s/<MYSQL_PASSWORD>/$MYSQL_PASSWORD/g" /etc/registry/registry.conf
+sed -i "s/<MYSQL_HOST>/${MYSQL_HOST:-mariadb}/g" /etc/registry/registry.conf
+sed -i "s/<EPP_SERVER_REPOSITORY>/${EPP_SERVER_REPOSITORY:-NIC}/g" /etc/registry/registry.conf
+sed -i "s/<EPP_SERVER_ID>/${EPP_SERVER_ID:-NIC Server}/g" /etc/registry/registry.conf
 
 # Start Apache in the foreground
 exec /usr/sbin/httpd -D FOREGROUND
